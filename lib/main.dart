e7e0bdeb85d5c4cf1e24d2c1ee53141999a6f593
leetreays <lee.treays@hotmail.co.uk>
2026-06-20 20:54:07 +0100

    Growth boost testing 40

--- lib/main.dart
+++ lib/main.dart
@@ -8038,7 +8038,7 @@ class JourneyScreen extends StatelessWidget {
     final growth =
     _treeGrowth(docs);
 
-    const journeyTestGrowthBoost = 0;
+    const journeyTestGrowthBoost = 40;
 
 final boostedGrowth = {
   ...growth,
