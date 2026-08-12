e268a78c9b8e076a465b06eb3c22d49129c0f89f
leetreays <lee.treays@hotmail.co.uk>
2026-08-12 20:32:21 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15969,7 +15969,7 @@ class DigitalReadinessReportScreen extends StatelessWidget {
           const SizedBox(width: 14),
           const Expanded(
             child: Text(
-              "$childName’s readiness journey",
+              '$childName’s readiness journey',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 20,
